b0fbc1c067b79095f749d7103a6b23bfc3d653ea
alvarosaavedrau <cloud@alvarosaavedra.es>
2025-01-04 17:58:01 +0100

    upload good code get public ip

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+addEventListener('fetch', event => {
+	const { request } = event;
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+	if (request.method === 'GET') {
+		event.respondWith(getClientIP(request));
+	} else {
+		event.respondWith(sendErrorResponse(`Method not allowed: ${request.method}`, 405));
+	}
+});
+
+async function getClientIP(request) {
+	const clientIP = request.headers.get('CF-Connecting-IP') || 'IP not found';
+
+	return new Response(clientIP, {
+		status: 200,
+		headers: {
+			'Content-Type': 'text/plain; charset=utf-8',
+			'Cache-Control': 'no-cache, no-store, must-revalidate',
+			'X-Content-Type-Options': 'nosniff',
+		}
+	});
+}
+
+function sendErrorResponse(message, statusCode = 400) {
+	const errorResponse = { error: message };
+
+	return new Response(JSON.stringify(errorResponse), {
+		status: statusCode,
+		headers: {
+			'Content-Type': 'application/json; charset=utf-8',
+			'Cache-Control': 'no-cache, no-store, must-revalidate',
+		}
+	});
+}
